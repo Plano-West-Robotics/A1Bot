dc13e05b56456a8daa1ee87dbf982d9034b4dce4
Akhil Kasamsetty <56555658+akhilk999@users.noreply.github.com>
2023-01-22 23:34:21 +0000

    tried debugging main.py since bot won't start

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import os
 import discord
-from keep_alive import keep_alive
+from discord.ext import commands
+from discord.utils import find
+#from keep_alive import keep_alive
 
-intents = discord.Intents.default()
+token = os.environ['TOKEN']
+
+intents = discord.Intents.all()
+intents.members = True
 intents.message_content = True
+intents.typing = False
+intents.presences = False
+intents.reactions = True
 
-client = discord.Client(intents=intents)
-prefix = os.environ['PREFIX']
+#client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='$', activity = discord.Game(name="$help"), intents=intents)
 
 @client.event
 async def on_ready():
@@ -17,11 +25,13 @@ async def on_message(message):
   if message.author == client.user:
     return 
 
+'''
   if message.content == prefix+'help':
     await message.channel.send('Hi! I am A1Bot, this year\'s robot for Free of            Charge#14523! These are a list of commands I can use: ')
 
   if message.content == 'onh':
     await message.add_reaction('\U0001F451')
+'''
 
-keep_alive()
-client.run(os.environ['TOKEN'])
+#keep_alive()
+client.run(token)
