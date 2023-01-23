2d2a352e14db45ffa2682e2d6c447b3302876503
Akhil Kasamsetty <56555658+akhilk999@users.noreply.github.com>
2023-01-23 06:11:48 +0000

    added functionality for reactions when message sent by Hemanth#8456 in #announcements, snake reaction when sending "jasper"

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 from discord.ext import commands
 from discord.utils import find
-#from keep_alive import keep_alive
+from keep_alive import keep_alive
 
 token = os.environ['TOKEN']
 
@@ -15,6 +15,7 @@ intents.reactions = True
 
 #client = discord.Client(intents=intents)
 client = commands.Bot(command_prefix='$', activity = discord.Game(name="$help"), intents=intents)
+client.remove_command('help')
 
 @client.event
 async def on_ready():
@@ -24,17 +25,22 @@ async def on_ready():
 async def on_message(message): 
   if message.author == client.user:
     return 
-
-
-  '''if message.content == prefix+'help':
-    await message.channel.send('Hi! I am A1Bot, this year\'s robot for Free of            Charge#14523! These are a list of commands I can use: ')'''
-
-  if message.content == 'onh':
+  if message.content.lower().startswith('onh'):
     await message.add_reaction('\U0001F451')
+  #announcements id & Hemanth967#8456 id
+  if message.channel.id == 995913953542668328 and message.author.id == 526054276355850266:
+    await message.add_reaction('\U0001F1F4')
+    await message.add_reaction('\U0001F1F3')
+    await message.add_reaction('\U0001F1ED')
+    await message.add_reaction('\U0001F451')
+  if message.content.lower().startswith('jasper'):
+    await message.add_reaction('\U0001F40D')
+  # process commands
+  await client.process_commands(message)
 
-@client.command
+@client.command()
 async def help(ctx):
   await ctx.send('Hi! I am A1Bot, this year\'s robot for Free of Charge#14523! These are a list of commands I can use: ')
 
-#keep_alive()
+keep_alive()
 client.run(token)
